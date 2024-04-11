ac71b4c4e1bac0065ce9005436fc914d8891bf44
p <pp@p.p>
2024-04-11 20:08:04 +0800

    delete

--- demos/train/src/worker.js
+++
@@ -1,2 +0,0 @@
-import { checkVersion } from './api';
-checkVersion();
--- packages/prefetch-core/nore.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (config) {
-  // console.log('prefetch config', config.input.plugins);
-  // throw new Error('prefetch config error');
-  return config;
-}
