2f20eca61282b52799471e53bd67ad51f6da0b64
p <p@p.com>
2024-05-11 08:22:03 +0800

    信息脱敏

--- README.md
+++ README.md
@@ -10,9 +10,6 @@ pnpm install @norejs/prefetch
 ```javascript
 // 发起预请求
 import { createPreRequest } from '@norejs/prefetch';
-
-// res 同@cotrip/corp-cross-request 的res
-// req 同@cotrip/corp-cross-request 的req
 // config config.expireTime 为缓存时间
 createPreRequest(res,req,config).post("/restapi/restapi", requestParams)
 
--- apps/demo/src/routes/page.tsx
+++ apps/demo/src/routes/page.tsx
@@ -1,90 +1,85 @@
-import { preRequest } from "@norejs/prefetch";
+// const customPreRequest = () => {
+//   const customerServiceSwitchModel = new RequestModel({
+//     isCorpRestApi: true,
+//     serviceName: "CorpFrontendIMService",
+//     serviceOperation: "customerServiceSwitch",
+//   });
 
-import { useEffect } from "react";
-import RequestModel from "@cotrip/ctweb-util/esm/ctmodel";
+//   const customerServiceSwitch = async (params: any) => {
+//     try {
+//       const response = await customerServiceSwitchModel.fetch({
+//         body: { params },
+//         silence: true,
+//       });
+//       return response;
+//     } catch (error) {
+//       return null;
+//     }
+//   };
+//   customerServiceSwitch({
+//     productLine: "4",
+//     pageCode: "DC0102",
+//     Channel: "html5",
+//     resourceSink: true,
+//     SecondaryChannel: "Other",
+//   }).then((res) => {
+//     console.log("customerServiceSwitch", res);
+//   });
+// };
 
-const customPreRequest = () => {
-  const customerServiceSwitchModel = new RequestModel({
-    isCorpRestApi: true,
-    serviceName: "CorpFrontendIMService",
-    serviceOperation: "customerServiceSwitch",
-  });
+// const language = "zh-CN";
+// const Index = () => {
+//   const requestParams = {
+//     params: {
+//       language,
+//     },
+//     operation: "getAllCategoricalCountryCode",
+//     language,
+//     serviceName: "CorpNodeCommon",
+//   };
+//   useEffect(() => {
+//     // PrefetchRoot.init(rules);
+//     let clock: any = 0;
+//     preRequest(undefined, undefined)
+//       .post("/restapi/restapi", requestParams)
+//       .then((res) => {
+//         console.log("prefetch request 这是预请求", res);
+//       });
 
-  const customerServiceSwitch = async (params: any) => {
-    try {
-      const response = await customerServiceSwitchModel.fetch({
-        body: { params },
-        silence: true,
-      });
-      return response;
-    } catch (error) {
-      return null;
-    }
-  };
-  customerServiceSwitch({
-    productLine: "4",
-    pageCode: "DC0102",
-    Channel: "html5",
-    resourceSink: true,
-    SecondaryChannel: "Other",
-  }).then((res) => {
-    console.log("customerServiceSwitch", res);
-  });
-};
+//     customPreRequest();
+//     clock = setTimeout(() => {
+//       realRequest();
+//     }, 2000);
+//     return () => {
+//       clearTimeout(clock);
+//     };
+//   }, []);
 
-const language = "zh-CN";
-const Index = () => {
-  const requestParams = {
-    params: {
-      language,
-    },
-    operation: "getAllCategoricalCountryCode",
-    language,
-    serviceName: "CorpNodeCommon",
-  };
-  useEffect(() => {
-    // PrefetchRoot.init(rules);
-    let clock: any = 0;
-    preRequest(undefined, undefined)
-      .post("/restapi/restapi", requestParams)
-      .then((res) => {
-        console.log("prefetch request 这是预请求", res);
-      });
+//   const realRequest = () => {
+//     console.time("real request");
+//     creatrerequest()
+//       .post("/restapi/restapi", requestParams)
+//       .then((res) => {
+//         console.log("real request 这是真实请求", res);
+//         console.timeEnd("real request");
+//       });
+//   };
 
-    customPreRequest();
-    clock = setTimeout(() => {
-      realRequest();
-    }, 2000);
-    return () => {
-      clearTimeout(clock);
-    };
-  }, []);
+//   return (
+//     <div className="container-box w-full">
+//       {/* <PrefetchRoot appUrl="/webapp/hailing/search" rules={rules}>
+//       <a>打车</a>
+//     </PrefetchRoot> */}
+//       <iframe
+//         width="100%"
+//         height={"100%"}
+//         frameBorder={0}
+//         style={{ border: 0 }}
+//         allowFullScreen={true}
+//         src=""
+//       />
+//     </div>
+//   );
+// };
 
-  const realRequest = () => {
-    console.time("real request");
-    creatrerequest()
-      .post("/restapi/restapi", requestParams)
-      .then((res) => {
-        console.log("real request 这是真实请求", res);
-        console.timeEnd("real request");
-      });
-  };
-
-  return (
-    <div className="container-box w-full">
-      {/* <PrefetchRoot appUrl="/webapp/hailing/search" rules={rules}>
-      <a>打车</a>
-    </PrefetchRoot> */}
-      <iframe
-        width="100%"
-        height={"100%"}
-        frameBorder={0}
-        style={{ border: 0 }}
-        allowFullScreen={true}
-        src=""
-      />
-    </div>
-  );
-};
-
-export default Index;
+// export default Index;
