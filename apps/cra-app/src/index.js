a66901445746bf429fe11e2fd7eb0a7ba9808d87
pp <pp@pp.p>
2024-07-12 15:02:49 +0800

    add demo

--- apps/cra-app/src/index.js
+++ apps/cra-app/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import(/* webpackChunkName: "some-module" */ "./prefetch.jsx").then(
+  (someModule) => {
+    // 使用 someModule
+    console.log(someModule);
+  }
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
