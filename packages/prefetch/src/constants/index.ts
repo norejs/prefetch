aedcafe4dfd23f799cfc932bb94601a1cd4bd851
pengzai <roc.liu@hotmail.com>
2025-08-16 16:05:10 +0800

    简化项目

--- packages/prefetch/src/constants/index.ts
+++ packages/prefetch/src/constants/index.ts
@@ -2,5 +2,4 @@ export const defaultExpireTime = 5000;
 export const headName = "X-Prefetch-Request-Type";
 export const headValue = "prefetch";
 export const expireTimeHeadName = "X-Prefetch-Expire-Time";
-export const serviceWorkerUrl = "/prefetch-worker/service-worker.js";
 export const scope = "/";
--- packages/prefetch/src/core/preRequest.ts
+++ packages/prefetch/src/core/preRequest.ts
@@ -3,13 +3,9 @@ import {
     headValue,
     expireTimeHeadName,
     defaultExpireTime,
-} from '../constants';
+} from "../constants";
 
-import setupPrefetchWorker from './setupPrefetchWorker';
-import logger from '@/utils/logger';
-import { scope, serviceWorkerUrl } from '@/constants';
-
-const requestMethods = ['get', 'post'];
+import logger from "../utils/logger";
 
 type IPreFetchOptions = {
     // 预取请求的有效时间
@@ -17,27 +13,29 @@ type IPreFetchOptions = {
 };
 
 export default function prefetch() {
-  // 利用fetch 封装一个预取请求
-  return async function preFetch(
-    url: string,
-    options: IPreFetchOptions = {}
-  ): Promise<void> {
-    const { expireTime = defaultExpireTime } = options;
-    const request = new Request(url, {
-      method: 'GET',
-      headers: new Headers({
-        [headName]: headValue,
-        [expireTimeHeadName]: expireTime.toString(),
-      }),
-    });
+    // 利用fetch 封装一个预取请求
+    return async function preFetch(
+        url: string,
+        options: IPreFetchOptions = {}
+    ): Promise<void> {
+        const { expireTime = defaultExpireTime } = options;
+        const request = new Request(url, {
+            method: 'GET',
+            headers: new Headers({
+                [headName]: headValue,
+                [expireTimeHeadName]: expireTime.toString(),
+            }),
+        });
 
-    try {
-      const response = await fetch(request);
-      if (response.status !== 200) {
-        throw new Error(`fetch ${url} failed, status: ${response.status}`);
-      }
-    } catch (e) {
-      logger.error(`prefetch ${url} failed`, e);
-    }
-  };
+        try {
+            const response = await fetch(request);
+            if (response.status !== 200) {
+                throw new Error(
+                    `fetch ${url} failed, status: ${response.status}`
+                );
+            }
+        } catch (e) {
+            logger.error(`prefetch ${url} failed`, e);
+        }
+    };
 }
